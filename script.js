dfb77ea2383708960b527b2aba81d208fe810302
Frank <fgrullon@users.noreply.github.com>
2017-06-02 14:39:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,27 @@ $(function(){
 
 		count++;
 
-		if(count === 9){
-			arr = test(document.querySelectorAll(".key"));
+		//This test on every move if the there if a winner or if the count is 9 if not it continues
+		var eval = test(document.querySelectorAll(".key"));
+		if(check(eval).length > 1 && check(eval)[1] != ""  || count === 9){
+			evaluate();
+			}
+		
+		
+		
+	});
+
+	$(".reset").click(function(){
+		location.reload();
+	});
+	
+
+});
+
+
+function evaluate(){
+		arr = test(document.querySelectorAll(".key"));
+console.log();
 
 			if(check(arr).length > 1){
 
@@ -34,23 +53,14 @@ $(function(){
 				$(".result")[0].style.display = 'block'; 
 				$(".container").addClass("disabled");
 				
-			}else{
+			}else if(!check(arr)[0]){
 				document.getElementsByClassName("result")[0].childNodes[3].innerText =  "Draw";
-				return "Draw";
+				$(".result")[0].style.display = 'block'; 
+				$(".container").addClass("disabled");
 
 
 			}
-		}
-		
-		
-	});
-
-	$(".reset").click(function(){
-		location.reload();
-	});
-
-});
-
+}
 
 function test(arr){
 	var arreglo = [];
@@ -66,35 +76,35 @@ function check(exp){
 	var result = [];
 	result.push(false);
 
-	    if ((exp[0].localeCompare(exp[3]) + exp[0].localeCompare(exp[6])) === 0){
+	    if ((exp[0].localeCompare(exp[3]) === 0 && exp[0].localeCompare(exp[6])) === 0 && exp[0] != ""){
 	    	result.push(exp[0]);
 	    	result[0] = true;
 
-	    }else if((exp[1].localeCompare(exp[4]) + exp[1].localeCompare(exp[7])) === 0){
+	    }else if((exp[1].localeCompare(exp[4]) === 0 && exp[1].localeCompare(exp[7])) === 0 && exp[1] != ""){
 	    	result.push(exp[1]);
 	    	result[0] = true;
 
-	    }else if ((exp[2].localeCompare(exp[5]) + exp[2].localeCompare(exp[8])) === 0){
+	    }else if ((exp[2].localeCompare(exp[5]) === 0 && exp[2].localeCompare(exp[8])) === 0 && exp[2] != ""){
 	        result.push(exp[2]);
 	    	result[0] = true;
 
-	    }else if ((exp[0].localeCompare(exp[4]) + exp[0].localeCompare(exp[8])) === 0){
+	    }else if ((exp[0].localeCompare(exp[4]) === 0 && exp[0].localeCompare(exp[8])) === 0 && exp[0] != ""){
 	    	result.push(exp[0]);
 	    	result[0] = true;
 
-	    }else if ((exp[6].localeCompare(exp[7]) + exp[6].localeCompare(exp[8])) === 0){
+	    }else if ((exp[6].localeCompare(exp[7]) === 0 && exp[6].localeCompare(exp[8])) === 0 && exp[6] != ""){
 	    	result.push(exp[6]);
 	    	result[0] = true;
 
-	    }else if ((exp[3].localeCompare(exp[4]) + exp[3].localeCompare(exp[5])) === 0){
+	    }else if ((exp[3].localeCompare(exp[4]) === 0 &&  exp[3].localeCompare(exp[5])) === 0 && exp[3] != ""){
 	    	result.push(exp[3]);
 	    	result[0] = true;
 
-	    }else if ((exp[0].localeCompare(exp[1]) + exp[0].localeCompare(exp[2])) === 0){ 
+	    }else if ((exp[0].localeCompare(exp[1]) === 0 && exp[0].localeCompare(exp[2])) === 0 && exp[0] != ""){ 
 	    	result.push(exp[0]);
 	    	result[0] = true;
 
-	    }else if ((exp[6].localeCompare(exp[4]) + exp[6].localeCompare(exp[2])) === 0){
+	    }else if ((exp[6].localeCompare(exp[4]) === 0 && exp[6].localeCompare(exp[2])) === 0 && exp[6] != ""){
 	    	result.push(exp[6]);
 	    	result[0] = true;
 	    }
