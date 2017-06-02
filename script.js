8dff8848ad72bc1b6ab70bae6002056da317b709
Frank <fgrullon@users.noreply.github.com>
2017-06-02 14:24:29 -0400

    Changes
    Add the validation function using some if and returning an array with a boolean and the simbol that win.
    
    Add some changes for the behavior of the page when the game finish to disable click on the board.
    
    Add the behavior for the result div that shows the simbol that wins and brings a restart button, to begin again.

--- script.js
+++ script.js
@@ -27,12 +27,17 @@ $(function(){
 		if(count === 9){
 			arr = test(document.querySelectorAll(".key"));
 
-			if(check(arr)){
-				console.log("Win");
-				return "Win";
+			if(check(arr).length > 1){
+
+				document.getElementsByClassName("result")[0].childNodes[3].innerText =  check(arr)[1]+" : Win";
+
+				$(".result")[0].style.display = 'block'; 
+				$(".container").addClass("disabled");
+				
 			}else{
-				console.log("Draw");
+				document.getElementsByClassName("result")[0].childNodes[3].innerText =  "Draw";
 				return "Draw";
+
 
 			}
 		}
@@ -40,9 +45,9 @@ $(function(){
 		
 	});
 
-
-	//var arr = document.querySelectorAll(".key")
-	//console.log(arr[0].innerText);
+	$(".reset").click(function(){
+		location.reload();
+	});
 
 });
 
@@ -58,44 +63,45 @@ function test(arr){
 }
 
 function check(exp){
-	var result = false;
-	console.log("hi");
-	
-	switch(exp) {
+	var result = [];
+	result.push(false);
 
-	    case ((exp[0].localeCompare(exp[3]) + exp[0].localeCompare(exp[6])) === 0):
-	        result = true;
-	        break;
+	    if ((exp[0].localeCompare(exp[3]) + exp[0].localeCompare(exp[6])) === 0){
+	    	result.push(exp[0]);
+	    	result[0] = true;
 
-	    case ((exp[1].localeCompare(exp[4]) + exp[1].localeCompare(exp[7])) === 0):
-	        result = true;
-	        break;
+	    }else if((exp[1].localeCompare(exp[4]) + exp[1].localeCompare(exp[7])) === 0){
+	    	result.push(exp[1]);
+	    	result[0] = true;
 
-	        case ((exp[2].localeCompare(exp[5]) + exp[2].localeCompare(exp[8])) === 0):
-	        result = true;
-	        break;
+	    }else if ((exp[2].localeCompare(exp[5]) + exp[2].localeCompare(exp[8])) === 0){
+	        result.push(exp[2]);
+	    	result[0] = true;
 
-	        case ((exp[0].localeCompare(exp[4]) + exp[0].localeCompare(exp[8])) === 0):
-	        result = true;
-	        break;
+	    }else if ((exp[0].localeCompare(exp[4]) + exp[0].localeCompare(exp[8])) === 0){
+	    	result.push(exp[0]);
+	    	result[0] = true;
 
-	        case ((exp[6].localeCompare(exp[7]) + exp[6].localeCompare(exp[8])) === 0):
-	        result = true;
-	        break;
+	    }else if ((exp[6].localeCompare(exp[7]) + exp[6].localeCompare(exp[8])) === 0){
+	    	result.push(exp[6]);
+	    	result[0] = true;
 
-	        case ((exp[3].localeCompare(exp[4]) + exp[3].localeCompare(exp[5])) === 0):
-	        result = true;
-	        break;
+	    }else if ((exp[3].localeCompare(exp[4]) + exp[3].localeCompare(exp[5])) === 0){
+	    	result.push(exp[3]);
+	    	result[0] = true;
 
-	        case ((exp[0].localeCompare(exp[1]) + exp[0].localeCompare(exp[2])) === 0):  
-	        result = true;
-	        break;
-	        
-	        case ((exp[6].localeCompare(exp[4]) + exp[6].localeCompare(exp[2])) === 0):
-	        result = true;
-	        break;
+	    }else if ((exp[0].localeCompare(exp[1]) + exp[0].localeCompare(exp[2])) === 0){ 
+	    	result.push(exp[0]);
+	    	result[0] = true;
+
+	    }else if ((exp[6].localeCompare(exp[4]) + exp[6].localeCompare(exp[2])) === 0){
+	    	result.push(exp[6]);
+	    	result[0] = true;
+	    }
+
+	    return result;	
 	}
-	return result;	
-}
+
+
 
 
